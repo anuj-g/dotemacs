fda139475de211725c60d4dbf098f05977623a68
Anuj Gandhi <Anuj Gandhi>
2016-09-17 03:26:08 -0700

    Add new theme zenburn-emacs

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
 ;;; init.el - Anuj Gandhi
 
 ;;;Code
+
+;;custom load paths
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn-emacs")
+
+;;load-theme
+(defvar zenburn-override-colors-alist
+  '(("zenburn-bg+05" . "#282828")
+    ("zenburn-bg+1"  . "#2F2F2F")
+    ("zenburn-bg+2"  . "#3F3F3F")
+    ("zenburn-bg+3"  . "#4F4F4F")))
+(load-theme 'zenburn t)
